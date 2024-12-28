684e02ad31b3c93411fe752ae0ab07d10c3e7dd4
东方天意 <dfty@dftianyi.com>
2024-12-28 09:38:21 +0800

    Update create_translation.R

--- R/create_translation.R
+++ R/create_translation.R
@@ -95,7 +95,10 @@ create_translation <- function(
 
     task <- "audio/translations"
 
-    base_url <- glue::glue("https://api.openai.com/v1/{task}")
+    base_url <- Sys.getenv("OPENAI_API_BASE_URL")
+        print(base_url)
+
+    base_url <- glue::glue("{base_url}/{task}")
 
     headers <- c(
         "Authorization" = paste("Bearer", openai_api_key),
