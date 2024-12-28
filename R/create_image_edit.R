a209fa916c2415a943af1b134f5c028d3a436669
东方天意 <dfty@dftianyi.com>
2024-12-28 09:35:49 +0800

    Update create_image_edit.R

--- R/create_image_edit.R
+++ R/create_image_edit.R
@@ -112,7 +112,10 @@ create_image_edit <- function(
 
     task <- "images/edits"
 
-    base_url <- glue::glue("https://api.openai.com/v1/{task}")
+    base_url <- Sys.getenv("OPENAI_API_BASE_URL")
+        print(base_url)
+
+    base_url <- glue::glue("{base_url}/{task}")
 
     headers <- c(
         "Authorization" = paste("Bearer", openai_api_key),
