9406c2e33d935a9b13af2cb9a4441b0c97d04c69
东方天意 <dfty@dftianyi.com>
2024-12-28 10:16:25 +0800

    Update retrieve_file.R

--- R/retrieve_file.R
+++ R/retrieve_file.R
@@ -50,7 +50,10 @@ retrieve_file <- function(
     #---------------------------------------------------------------------------
     # Build parameters of the request
 
-    base_url <- glue::glue("https://api.openai.com/v1/files/{file_id}")
+    base_url <- Sys.getenv("OPENAI_API_BASE_URL")
+        print(base_url)
+
+    base_url <- glue::glue("{base_url}/files/{file_id}")
 
     headers <- c(
         "Authorization" = paste("Bearer", openai_api_key),
