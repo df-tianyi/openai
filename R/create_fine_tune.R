2421e5e186b65c8594fb720644f30a5a68eda252
东方天意 <dfty@dftianyi.com>
2024-12-28 09:34:16 +0800

    Update create_fine_tune.R

--- R/create_fine_tune.R
+++ R/create_fine_tune.R
@@ -155,7 +155,10 @@ create_fine_tune <- function(
 
     task <- "fine-tunes"
 
-    base_url <- glue::glue("https://api.openai.com/v1/{task}")
+    base_url <- Sys.getenv("OPENAI_API_BASE_URL")
+        print(base_url)
+
+    base_url <- glue::glue("{base_url}/{task}")
 
     headers <- c(
         "Authorization" = paste("Bearer", openai_api_key),
