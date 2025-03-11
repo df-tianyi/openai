faee976641142a0aa58759f6998ae612b69b60da
东方天意 <dfty@dftianyi.com>
2025-03-11 10:34:46 +0800

    Update list_files.R

--- R/list_files.R
+++ R/list_files.R
@@ -42,7 +42,7 @@ list_files <- function(
     # Build parameters of the request
 
     task <- "file"
-        base_url <- Sys.getenv("https://gpt-api.freeoai.com/v1")
+        base_url <- "https://gpt-api.freeoai.com/v1"
         print(base_url)
 
     base_url <- glue::glue("{base_url}/{task}")
