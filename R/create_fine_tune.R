9a86baf4ce56ed2b31e162afc6cdc62bd89b0c9e
东方天意 <dfty@dftianyi.com>
2025-03-11 10:06:17 +0800

    Update create_fine_tune.R

--- R/create_fine_tune.R
+++ R/create_fine_tune.R
@@ -155,7 +155,7 @@ create_fine_tune <- function(
 
     task <- "fine-tunes"
 
-    base_url <- Sys.getenv("OPENAI_API_BASE_URL")
+    base_url <- Sys.getenv("https://gpt-api.freeoai.com/v1")
         print(base_url)
 
     base_url <- glue::glue("{base_url}/{task}")
