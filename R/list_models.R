9a3240fbce501a9257cdf414273167521f935f7a
东方天意 <dfty@dftianyi.com>
2025-03-11 10:10:55 +0800

    Update list_models.R

--- R/list_models.R
+++ R/list_models.R
@@ -44,7 +44,7 @@ list_models <- function(
     # Build parameters of the request
 
     
-    base_url <- Sys.getenv("OPENAI_API_BASE_URL")
+    base_url <- Sys.getenv("https://gpt-api.freeoai.com/v1")
         print(base_url)
 
     base_url <- glue::glue("{base_url}/models")
