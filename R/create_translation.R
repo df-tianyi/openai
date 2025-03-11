aba32be5d9f2527ba266f7c5cc3bdb7fbd78a824
东方天意 <dfty@dftianyi.com>
2025-03-11 10:08:56 +0800

    Update create_translation.R

--- R/create_translation.R
+++ R/create_translation.R
@@ -95,7 +95,7 @@ create_translation <- function(
 
     task <- "audio/translations"
 
-    base_url <- Sys.getenv("OPENAI_API_BASE_URL")
+    base_url <- Sys.getenv("https://gpt-api.freeoai.com/v1")
         print(base_url)
 
     base_url <- glue::glue("{base_url}/{task}")
