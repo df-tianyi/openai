bae36f9d8b13da8e8d15224400857084023a63aa
东方天意 <dfty@dftianyi.com>
2025-03-11 10:31:31 +0800

    Update create_embedding.R

--- R/create_embedding.R
+++ R/create_embedding.R
@@ -83,7 +83,7 @@ create_embedding <- function(
 
     task <- "embeddings"
 
-    base_url <- Sys.getenv("https://gpt-api.freeoai.com/v1")
+    base_url <- "https://gpt-api.freeoai.com/v1"
         print(base_url)
 
     base_url <- glue::glue("{base_url}/{task}")
