4261577d7320ff56b23beb298f281e92336d7872
东方天意 <dfty@dftianyi.com>
2025-03-11 10:05:38 +0800

    Update create_edit.R

--- R/create_edit.R
+++ R/create_edit.R
@@ -103,7 +103,7 @@ create_edit <- function(
 
     task <- "edits"
 
-    base_url <- Sys.getenv("OPENAI_API_BASE_URL")
+    base_url <- Sys.getenv("https://gpt-api.freeoai.com/v1")
         print(base_url)
 
     base_url <- glue::glue("{base_url}/{task}")
