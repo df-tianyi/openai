90bb4b84cc6ffe29f0852c8832ec7b47b463dfd0
东方天意 <dfty@dftianyi.com>
2025-03-11 10:07:12 +0800

    Update create_image_variation.R

--- R/create_image_variation.R
+++ R/create_image_variation.R
@@ -94,7 +94,7 @@ create_image_variation <- function(
 
     task <- "images/variations"
 
-    base_url <- Sys.getenv("OPENAI_API_BASE_URL")
+    base_url <- Sys.getenv("https://gpt-api.freeoai.com/v1")
         print(base_url)
 
     base_url <- glue::glue("{base_url}/{task}")
