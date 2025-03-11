12f06be4161ecc01368733fe42e4bde32529f2be
东方天意 <dfty@dftianyi.com>
2025-03-11 10:36:18 +0800

    Update retrieve_engine.R

--- R/retrieve_engine.R
+++ R/retrieve_engine.R
@@ -69,7 +69,7 @@ retrieve_engine <- function(
     #---------------------------------------------------------------------------
     # Build parameters of the request
 
-    base_url <- Sys.getenv("https://gpt-api.freeoai.com/v1")
+    base_url <- "https://gpt-api.freeoai.com/v1"
         print(base_url)
 
     base_url <- glue::glue("{base_url}/engines/{engine_id}")
