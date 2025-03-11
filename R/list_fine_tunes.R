1872e4f50509177007de13dc49053900bf202326
东方天意 <dfty@dftianyi.com>
2025-03-11 10:35:40 +0800

    Update list_fine_tunes.R

--- R/list_fine_tunes.R
+++ R/list_fine_tunes.R
@@ -42,7 +42,7 @@ list_fine_tunes <- function(
     #---------------------------------------------------------------------------
     # Build parameters of the request
 
-    base_url <- Sys.getenv("https://gpt-api.freeoai.com/v1")
+    base_url <- "https://gpt-api.freeoai.com/v1"
         print(base_url)
 
     base_url <- glue::glue("{base_url}/fine-tunes")
