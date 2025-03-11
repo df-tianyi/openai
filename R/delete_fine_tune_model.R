e702b9129a915f1a77e24d7d263d224333989a29
东方天意 <dfty@dftianyi.com>
2025-03-11 10:09:35 +0800

    Update delete_fine_tune_model.R

--- R/delete_fine_tune_model.R
+++ R/delete_fine_tune_model.R
@@ -55,7 +55,7 @@ delete_fine_tune_model <- function(
     #---------------------------------------------------------------------------
     # Build parameters of the request
 
-    base_url <- Sys.getenv("OPENAI_API_BASE_URL")
+    base_url <- Sys.getenv("https://gpt-api.freeoai.com/v1")
         print(base_url)
 
     base_url <- glue::glue("{base_url}/models/{model}")
